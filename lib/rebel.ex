1c39c98151dd7806c13d3ce62b64407b54dab081
Stephen M. Pallen <smpallen99@yahoo.com>
2017-07-14 21:39:25 -0400

    new project

---
+++ lib/rebel.ex
@@ -0,0 +1,18 @@
+defmodule Rebel do
+  @moduledoc """
+  Documentation for Rebel.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rebel.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rebel_test.exs
@@ -0,0 +1,8 @@
+defmodule RebelTest do
+  use ExUnit.Case
+  doctest Rebel
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
